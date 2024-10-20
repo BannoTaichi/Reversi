3ef83d36a558e29918e51a8182fc3d773e7c3568
Banno Taichi <taichib@icloud.com>
2024-10-20 14:12:38 +0900

    Create Borderline

--- app.py
+++ app.py
@@ -10,4 +10,9 @@ while True:
         if e.type == pygame.QUIT:
             exit()
     win.fill((0, 128, 0))
+    [
+        pygame.draw.rect(win, (0, 0, 0), (x * 50, y * 50, 50, 50), 1)
+        for x in range(8)
+        for y in range(8)
+    ]
     pygame.display.flip()
